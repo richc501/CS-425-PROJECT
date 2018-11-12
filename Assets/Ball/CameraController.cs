90d6148bd540e046527a2ae1d64b5d61c3337f02
Nicholas Anderson <justalilbitnerdy@gmail.com>
2018-11-11 22:14:28 -0500

    started level spawning

---
+++ Assets/Ball/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject ball;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, ball.transform.position + new Vector3(4, 3, 0), step);
+        transform.rotation = Quaternion.LookRotation((transform.position - ball.transform.position + new Vector3(0, -2, 0)).normalized * -1);
+        //transform.SetPositionAndRotation(ball.transform.position + new Vector3(2, 2, 0), );
+    }
+}
