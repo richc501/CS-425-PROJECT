71bf47013c8e0657c5881b03d3ebf80df46e5c60
richc501 <richc501@gmail.com>
2018-11-26 13:19:07 -0500

    Added new level test

---
+++ Assets/Scripts/rotateCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCube : MonoBehaviour {
+
+    // Use this for initialization
+    int r;
+	void Start () {
+		r = Random.Range(0, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        switch(r)
+        {
+            case 0:
+                transform.Rotate(Vector3.right * 0.5f);
+                break;
+            case 1:
+                transform.Rotate(Vector3.left * 0.5f);
+                break;
+        }
+        
+	}
+}
