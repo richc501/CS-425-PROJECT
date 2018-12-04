0be5d6c86342928a361099439c6ab36ea53d2f49
richc501 <richc501@gmail.com>
2018-12-04 17:15:24 -0500

    Added a minimap
    Still needs icons for goals

---
+++ Assets/updateIcon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateIcon : MonoBehaviour {
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
