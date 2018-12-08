844cd3b16cb96fe36e0d0c8064177ae190397e3c
Nicholas Anderson <justalilbitnerdy@gmail.com>
2018-12-08 18:08:58 -0500

    Made moving platforms

---
+++ Assets/Scripts/Movement/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime);
+	}
+}
