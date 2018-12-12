7fb7bb0dbcff9524058df2aba7cdd51b612fa2d9
Nicholas Anderson <justalilbitnerdy@gmail.com>
2018-12-12 12:09:08 -0500

    finished turret

---
+++ Assets/Scripts/Movement/TurretMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMovement : MonoBehaviour {
+    //destination, using offsets
+    public float speed;
+    private Quaternion left;
+    private Quaternion right;
+    private Quaternion nextRot;
+    private Quaternion nextTilt;
+    private bool spotted;
+    private GameObject target;
+    public GameObject turret;
+    public GameObject gun;
+
+    // Use this for initialization
+    void Start(){
+        spotted = false;
+        nextTilt = Quaternion.AngleAxis(Random.Range(-30,30), Vector3.up);
+        left = turret.transform.rotation * Quaternion.AngleAxis(90, Vector3.up);
+        right = turret.transform.rotation * Quaternion.AngleAxis(-90, Vector3.up);
+        nextRot = left;
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float step = speed * Time.deltaTime;
+        Vector3 targetDir = target.transform.position - turret.transform.position;
+        float angle = Vector3.Angle(targetDir, turret.transform.forward);
+        if (angle < 40) {
+            spotted = true;
+        } else {
+            spotted = false;
+        }
+
+        if (turret.transform.rotation.y <= 0 || gun.transform.rotation.z <= -2 || gun.transform.rotation.z >= 2)
+            spotted = false;
+
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, target.transform.position, out hit)) {
+            if (hit.transform.tag != "Player")
+                spotted = false;
+        }
+        rotateGun(step);
+        tiltGun(step);
+        if(spotted)
+            shoot();
+    }
+
+    void shoot(){
+
+    }
+
+    void rotateGun(float step){
+        
+        if (spotted)
+        {
+            var lookDir = target.transform.position - turret.transform.position;
+            lookDir.y = 0; // keep only the horizontal direction
+            turret.transform.rotation = Quaternion.LookRotation(lookDir);
+        }
+        else
+        {
+            if (turret.transform.rotation == left)
+                nextRot = right;
+            else if (turret.transform.rotation == right)
+                nextRot = left;
+
+            turret.transform.rotation = Quaternion.RotateTowards(turret.transform.rotation, nextRot, step);
+        }
+        
+    }
+
+    void tiltGun(float step){
+        
+        if (spotted){
+            gun.transform.LookAt(target.transform.position);
+        }
+        else
+        {
+            if (gun.transform.rotation == nextTilt * turret.transform.rotation)
+                nextTilt = Quaternion.AngleAxis(Random.Range(-30, 30), Vector3.right);
+            gun.transform.rotation = Quaternion.RotateTowards(gun.transform.rotation, nextTilt * turret.transform.rotation, step);
+        }
+        
+    }
+
+    public void setSpotted(bool spotted){
+        this.spotted = spotted;
+    }
+}
